9a5a1d3a4c017325c63506002cd3b558404e6768
grenykssa <79082904+grenykssa@users.noreply.github.com>
2021-02-16 12:07:07 +0700

    Create nangisan.sh

---
+++ nangisan.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+green='\33[32;1m'
+blue='\33[0;36m'
+red='\33[31;1m'
+purple='\e[0;35m'
+Oren='\33[33;1m'
+black='\33[30;1m'
+white='\33[37;1m'
+yellow='\33[33;1m'
+
+clear
+
+toilet -f big -F gay fauzan ck
+echo 
+sleep 2
+clear
+
+echo  "\33[33;1m 
+       ======================================
+       SELAMAT DATANG DI TOOLS FAUZAN PRATMA
+       ======================================
+: | lolcat
+sleep 2
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay  password
+echo "\033[33;1m =================================="
+echo "\033[33;1m SILAHKAN MASUKAN PW : fauzanpratama"
+echo "\033[33;1m  WHATSAPP : 081276754581"
+echo "\033[33;1m  =================================="
+
+name="fauzanpratama"
+while [ "$adin" != "$name" ]
+do
+  read -p "MASUKAN PASSWORD BRO:)" adin
+done
+  echo "OK PASSWORD ANDA BENAR..."
+  sleep 2
+  clear   
+  
+  blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay welcome
+
+echo "\033[32;1m ============================"
+echo "\033[32;1m       AUTHOR : FAUZAN PRATAMA"
+echo "\033[32;1m       KONTAK : 082217836017"
+echo "\033[32;1m ============================"
+echo "\033[31;1m ===========SILAHKAN PILIH MENU==========="
+echo "\033[31;1m  (01) BUG TSEL WHATSAPP"
+echo "\033[31;1m  (02) BUG TSEL INSTAGRAM"
+echo "\033[31;1m  (03) BUG TSEL YOUTUBE"
+echo "\033[31;1m  (04) BUG TSEL KB BIASA"
+echo "\033[31;1m  (05) EXIT..."
+echo "\033[31;1m =========================================="
+echo "\033[37;1m <<<PILIH MENU>>>"
+read -p " [ PILIH NOMOR MENU ]>>>)" ; pilih
+if [ $pilih = "01" ]
+then
+clear
+toilet -f big -F gay bug wa
+echo "\033[32;1m Loading..."
+sleep 2
+echo "\033[31;1m ================"
+echo "\033[31;1m www.whatsapp.com"
+echo "\033[31;1m ================"
+sleep 2
+echo "\033[37;1m ++++(SNI)++++"
+elif [ $pilih = "02" ]
+then
+clear
+toilet -f big -F gay bug ig
+echo "\033[32;1m Loading..."
+sleep 2
+echo "\033[31;1m =============="
+echo "\033[31;1m m.instagram.com"
+echo "\033[31;1m =============="
+sleep 2
+echo "\033[37;1m ++++(SNI)++++"
+elif [ $pilih = "03" ]
+then
+clear
+toilet -f big -F gay bug yt
+echo "\033[32;1m Loading..."
+sleep 2
+echo "\033[31;1m ==============="
+echo "\033[31;1m www.youtube.com"
+echo "\033[31;1m ==============="
+sleep 2
+echo "\033[37;1m ++++(SNI)++++"
+elif [ $pilih = "04" ]
+then
+clear
+toilet -f big -F gay bug kb
+echo "\033[32;1m Loading..."
+sleep 2
+echo "\033[31;1m ============"
+echo "\033[31;1m  www.zoom.us"
+echo "\033[31;1m ============"
+sleep 2
+echo "\033[37;1m ++++(SNI)++++"
+elif [ $pilih = "05" ]
+then
+clear
+toilet -f big -F keluar...
+echo "\033[31;1m Loading..."
+echo "exiting..."
+sleep 3
+exit
+else
+echo "\033[37;1m input salah"
+sleep 2
+fi
